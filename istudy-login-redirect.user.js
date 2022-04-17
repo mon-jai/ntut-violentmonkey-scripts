be076b92d4d6c0dbe334cc29dd2bf1853dd1e105
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-04-17 16:49:14 +0800

    perf(istudy-login-redirect): run at `document-start`

--- istudy-login-redirect.user.js
+++ istudy-login-redirect.user.js
@@ -3,6 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://nportal.ntut.edu.tw/*
 // @grant       none
+// @run-at      document-start
 // @version     1.0
 // @author      Loh Ka Hong
 // @description Access iStudy with the link "https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_", and get redirected after successful login
@@ -10,16 +11,28 @@
 // ==/UserScript==
 
 {
+  function redirectIfLoginIsRequired(redirectedCallback) {
+    // `readystatechange` is only available on `document`
+    // https://www.w3.org/TR/2014/NOTE-html5-diff-20141209/#document-extensions
+    document.addEventListener("readystatechange", () => {
+      if (document.documentElement.outerHTML.includes("重新登入")) {
+        if (redirectedCallback) redirectedCallback()
+        document.location.replace("https://nportal.ntut.edu.tw/index.do")
+      }
+    })
+  }
+
   const pathname = document.location.pathname
 
-  if (pathname === "/ssoIndex.do" && document.querySelector('a[onclick="logout()"]')?.innerText === "重新登入") {
-    sessionStorage.setItem("redirect-to-istudy", true)
-    document.location.replace("https://nportal.ntut.edu.tw/index.do")
+  if (pathname === "/ssoIndex.do") {
+    redirectIfLoginIsRequired(() => sessionStorage.setItem("redirect-to-istudy", true))
   } else if (pathname === "/login.do") {
-    const loginAgainButton = document.querySelector("input[value=重新登入]")
-    if (loginAgainButton !== null) loginAgainButton.click()
-  } else if (pathname === "/myPortal.do" && sessionStorage.getItem("redirect-to-istudy") === "true") {
-    sessionStorage.removeItem("redirect-to-istudy")
-    document.location.replace("https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_")
+    redirectIfLoginIsRequired()
+
+    // The following code runs before DOM parsing, and therefore before any script tag is executed
+    if (sessionStorage.getItem("redirect-to-istudy") === "true") {
+      sessionStorage.removeItem("redirect-to-istudy")
+      document.location.replace("https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_")
+    }
   }
 }
