6f188e7b935c5da6fc20ad2ba67067b6b6f1b0d7
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-04-24 13:44:19 +0800

    .

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,15 +1,10 @@
-chrome.runtime.onMessage.addListener(async () => {
+chrome.runtime.onMessage.addListener(async (_message, _sender, sendResponse) => {
   // https://stackoverflow.com/a/71520415
-  // sendResponse()
+  sendResponse()
 
   const cookies = await chrome.cookies.getAll({ domain: "nportal.ntut.edu.tw" })
 
-  console.log(cookies)
-
   for (const cookie of cookies) {
-    console.log(cookie)
     await chrome.cookies.remove({ url: `https://${cookie.domain}${cookie.path}`, name: cookie.name })
   }
-
-  console.log(await chrome.cookies.getAll({ domain: "nportal.ntut.edu.tw" }))
 })
