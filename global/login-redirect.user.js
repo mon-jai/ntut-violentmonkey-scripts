668b7a935064066ccc7dad26464cf74952485501
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-11-06 16:47:30 +0800

    refactor(login-redirect): optimize code and update iStudy URL

--- global/login-redirect.user.js
+++ global/login-redirect.user.js
@@ -6,43 +6,50 @@
 // @run-at      document-start
 // @version     1.0
 // @author      Loh Ka Hong
-// @description Access NTUT Portal with "https://nportal.ntut.edu.tw/myPortal.do" and iStudy with "https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_", then get redirected after successful login
+// @description Access NTUT Portal with "https://nportal.ntut.edu.tw/myPortal.do" and iStudy with "https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_oauth", then get redirected after successful login
 // @homepage    https://github.com/mon-jai/ntut-violentmonkey-scripts
 // ==/UserScript==
 
 "use strict"
 
-function redirectIfLoginIsRequired() {
+async function redirectIfUnauthenticated() {
+  const isAuthenticated = await isAuthenticatedPromise
+  if (isAuthenticated === false) document.location.replace("https://nportal.ntut.edu.tw/index.do")
+}
+
+const isAuthenticatedPromise = new Promise(resolve => {
   // `readystatechange` is only available on `document`
   // https://www.w3.org/TR/2014/NOTE-html5-diff-20141209/#document-extensions
   document.addEventListener("readystatechange", () => {
-    if (document.documentElement.outerHTML.includes("重新登入")) {
-      document.location.replace("https://nportal.ntut.edu.tw/index.do")
-    }
+    if (document.documentElement.outerHTML.includes("重新登入")) resolve(false)
+    else resolve(true)
   })
-}
+})
+const { pathname } = document.location
 
-const pathname = document.location.pathname
+isAuthenticatedPromise.then(isAuthenticated => {
+  if (!isAuthenticated) document.documentElement.style.display = "none"
+})
 
 // Portal page
 if (pathname === "/myPortal.do") {
   sessionStorage.removeItem("redirect-to-istudy")
-  redirectIfLoginIsRequired()
+  redirectIfUnauthenticated()
 }
 // iStudy entry page
 else if (pathname === "/ssoIndex.do") {
   sessionStorage.setItem("redirect-to-istudy", true)
-  redirectIfLoginIsRequired()
+  redirectIfUnauthenticated()
 }
 // Login successful/unsuccessful page
 else if (pathname === "/login.do") {
   // If a "login again" message is displayed, redirect user to login page
-  redirectIfLoginIsRequired()
+  redirectIfUnauthenticated()
 
   // Redirect user to iStudy on successful login
   // The following code runs before DOM parsing, and therefore before any script tag is executed
   if (JSON.parse(sessionStorage.getItem("redirect-to-istudy")) === true) {
     sessionStorage.removeItem("redirect-to-istudy")
-    document.location.replace("https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_")
+    document.location.replace("https://nportal.ntut.edu.tw/ssoIndex.do?apOu=ischool_plus_oauth")
   }
 }
